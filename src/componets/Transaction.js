a780002ce6053b12b4bca6f905826d75fe8370cb
Marcin Gierada <gierada.marcin@gmail.com>
2021-02-19 12:16:13 +0100

    Add Prettier

--- src/componets/Transaction.js
+++ src/componets/Transaction.js
@@ -1,14 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
-export const Transaction = ({transaction}) => {
-    const { deleteTransaction } = useContext(GlobalContext)
-    
-    const sing = transaction.amount < 0 ? '-': '+';
+export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
 
-    return (
-        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-                {transaction.text} <span>{sing}${Math.abs(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id) } className="delete-btn">x</button>
-        </li>
-    )
-}
+  const sing = transaction.amount < 0 ? '-' : '+';
+
+  return (
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+      {transaction.text}{' '}
+      <span>
+        {sing}${Math.abs(transaction.amount)}
+      </span>
+      <button
+        onClick={() => deleteTransaction(transaction.id)}
+        className='delete-btn'
+      >
+        x
+      </button>
+    </li>
+  );
+};
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,41 +3,45 @@ import AppReducer from './AppReducer';
 
 // Initial state
 const initialState = {
-    transactions: [ 
-  { id: Date.now(), text: 'Book', amount: -20 },
-  { id: Date.now()+1, text: 'Salary', amount: 400 },
-  { id: Date.now()+2, text: 'Swimingpool', amount: -100 },
-  { id: Date.now()+3, text: 'Bonus Salary', amount: 150 }
-    ]
-}
+  transactions: [
+    { id: Date.now(), text: 'Book', amount: -20 },
+    { id: Date.now() + 1, text: 'Salary', amount: 400 },
+    { id: Date.now() + 2, text: 'Swimingpool', amount: -100 },
+    { id: Date.now() + 3, text: 'Bonus Salary', amount: 150 },
+  ],
+};
 
 // Create context
 export const GlobalContext = createContext(initialState);
 
 // Provider component
-export const GlobalProvider = ( {children} ) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState);
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    //Acction
-    function deleteTransaction(id){
-        dispatch({
-            type: 'DELETE_TRANSACTION',
-            payload: id
-        });
-    }
+  //Acction
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id,
+    });
+  }
 
-    function addTransaction(transaction){
-        dispatch({
-            type: 'ADD_TRANSACTION',
-            payload: transaction
-        });
-    }
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction,
+    });
+  }
 
-    return (<GlobalContext.Provider value={{
+  return (
+    <GlobalContext.Provider
+      value={{
         transactions: state.transactions,
         deleteTransaction,
-        addTransaction
-    }}>
-        {children}
-    </GlobalContext.Provider>);
-}
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
