572b583708c47f1396d760353c105a9cde87e153
MarcinMG-web <gierada.marcin@gmail.com>
2021-07-20 12:18:44 +0200

    Add TS in IncomeExpense

---
+++ src/components/IncomeExpenses.tsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+export const IncomeExpenses: React.FC = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts: number[] = transactions.map(
+    (transaction: { id: number; text: string; amount: number }) =>
+      transaction.amount
+  );
+
+  const income: string = amounts
+    .filter((item:number) => item > 0)
+    .reduce((acc:number, item:number) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense: string = (
+    amounts
+      .filter((item: number) => item < 0)
+      .reduce((acc: number, item: number) => (acc += item), 0) * -1
+  ).toFixed(2);
+
+  return (
+    <div className='inc-exp-container'>
+      <div>
+        <h4>Income</h4>
+        <p className='money plus'>+{income}</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p className='money minus'>-{expense}</p>
+      </div>
+    </div>
+  );
+};
